d47cf89cef73833cc2e28ccf7556917e0c657afe
Wendell Miyaji <wendellmiyaji@Wendells-MacBook-Air.local>
2018-03-21 18:02:58 -0400

    Added some clarifying comments

--- Natural Gas Hubs.R
+++ Natural Gas Hubs.R
@@ -13,14 +13,15 @@ all.states <- map_data("state")
 gas_hubs <- read.csv("./Data/Natural_Gas_Market_Hubs.csv", 
                header=TRUE, 
                stringsAsFactors=FALSE)
-pipelines <- st_read("./Data/Natural_Gas_Liquid_Pipelines.shp", quiet = TRUE)
-lower48 <- pipelines %>% filter(OperatorNa != "Kenai Kachemak Pipeline")
+pipelines <- st_read("./Data/Natural_Gas_Liquid_Pipelines/Natural_Gas_Liquid_Pipelines.shp", quiet = TRUE)
+lower48 <- pipelines %>% filter(OperatorNa != "Kenai Kachemak Pipeline") # one pipeline in Alaska throws off presentation
 
-p <- gas_hubs %>% ggplot(aes(x=X, y=Y)) + 
-  geom_polygon(data=all.states, aes(x=long, y=lat, group = group),colour=palette[3], fill=NA)+ 
-  geom_sf(data = lower48, inherit.aes =FALSE, color=palette[3]) +
-  geom_point(color=palette[1]) + 
-  geom_text_repel(aes(label=Hub_name), color="black") + 
+p <- ggplot() + 
+  geom_polygon(data=all.states, aes(x=long, y=lat, group = group),colour=palette[2], fill=NA)+ 
+  geom_sf(data = lower48,  color=palette[3]) +
+  coord_sf(datum = NA) +
+  geom_point(data = gas_hubs,aes(x=X, y=Y),color=palette[1]) + 
+  geom_text_repel(data = gas_hubs, aes(x=X, y=Y,label=Hub_name), color="black") + 
   labs(title='Natural Gas Hubs & Pipelines from HIFLD') + 
   theme_map() +
   theme(panel.border = element_blank(), panel.grid.major = element_blank(),
